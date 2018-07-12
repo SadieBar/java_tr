cc49f94626206c4088db81f28d5096cfa4c7b5ba
Sadie <barievasr@gmail.com>
2018-07-12 20:26:57 +0300

    Added Contact creation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String surname;
+  private final String nick;
+  private final String phone;
+  private final String email;
+
+  public ContactData(String name, String surname, String nick, String phone, String email) {
+    this.name = name;
+    this.surname = surname;
+    this.nick = nick;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public String getNick() {
+    return nick;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
