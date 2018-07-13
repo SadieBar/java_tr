32a7762362aae3856ea53450fa049803712693cf
Sadie <barievasr@gmail.com>
2018-07-13 14:57:59 +0300

    Moved method to BaseHelper

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/BaseHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/BaseHelper.java
@@ -1,4 +1,15 @@
 package ru.stqa.addressbook.appmanager;
 
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BaseHelper {
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
 }
