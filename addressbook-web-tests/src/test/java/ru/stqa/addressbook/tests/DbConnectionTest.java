fab12bbdcaf9380e0987db421fa365416f32f4d4
Sadie <barievasr@gmail.com>
2018-08-19 13:44:46 +0300

    Добавлен доступ в базы данных

---
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,39 @@
+package ru.stqa.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.addressbook.model.GroupData;
+import ru.stqa.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+  @Test
+  public void testDbConnection() {
+    Connection conn = null;
+    try {
+      conn =
+              DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?" +
+                      "user=root&password=&serverTimezone=UTC");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("SELECT group_id,group_name,group_header,group_footer FROM `group_list` ");
+      Groups groups = new Groups();
+      while (rs.next()){
+        groups.add(new GroupData().withId(rs.getInt("group_id"))
+        .withGroupName(rs.getString("group_name"))
+        .withHeader(rs.getString("group_header"))
+        .withFooter(rs.getString("group_footer"))
+        );
+      }
+      rs.close();st.close();conn.close();
+      System.out.println(groups);
+
+      // Do something with the Connection
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+}
