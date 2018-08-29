0277705adbed7f058986081701095b95abd3e884
Sadie <barievasr@gmail.com>
2018-08-29 21:29:56 +0300

    Добавлен DbHelper

---
+++ mantistests/src/test/java/ru/stqa/pft/mantis/model/UserData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.mantis.model;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@javax.persistence.Table(name="mantis_user_table")
+public class UserData {
+  @Id
+  @Column(name="username")
+  @Type(type = "text")
+  private String user;
+  @Column(name="email")
+  @Type(type = "text")
+  private String email;
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+
+  public String getUser() {return user;}
+  public void setUser(String set) {this.user=set;}
+}
