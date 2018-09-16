21a8c27758badbe8cc10e12c47172787a89dcc40
Sadie <barievasr@gmail.com>
2018-09-16 18:39:36 +0300

    added some things

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
@@ -1,9 +1,30 @@
-package ru.stqa.addressbook.appmanager;
+/*package ru.stqa.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import ru.stqa.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+*/
+package ru.stqa.addressbook.appmanager;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -34,27 +55,30 @@ public class ApplicationManager {
   public ApplicationManager(String browser) {
     this.browser = browser;
     properties = new Properties();
-    }
-
+  }
+  @AfterSuite(alwaysRun = false)
+  public void tearDown() {
+    app.stop();
+  }
 
   public void init() throws IOException {
     dbHelper = new DbHelper();
-    String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("addressbook-web-tests/src/test/resources/%s.properties", target))));
+    String target = System.getProperty("remote", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-    if ("").equals(properties.getProperty("selenium-server")) {
+    if ("".equals(properties.getProperty("selenium.server"))) {
       if (browser.equals(BrowserType.FIREFOX)) {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
       } else if (browser.equals(BrowserType.CHROME)) {
-        System.setProperty(properties.getProperty("chrome.name"), properties.getProperty("chrome.path"));
         wd = new ChromeDriver();
       } else if (browser.equals(BrowserType.IE)) {
         wd = new InternetExplorerDriver();
       }
     } else {
-      DesiredCapabilities capabilities = new DesiredCapabilities();
+      DesiredCapabilities  capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium-server")),capabilities);
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win8")));
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
@@ -87,3 +111,68 @@ public class ApplicationManager {
     return dbHelper;
   }
 }
+
+/*
+public class ApplicationManager {
+  private Properties properties;
+  WebDriver wd;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  private SessionHelper sessionHelper;
+  private ContactHelper contactHelper;
+  private String browser;
+  private DbHelper dbHelper;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+    }
+
+
+
+  public void init() throws IOException {
+    dbHelper = new DbHelper();
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("addressbook-web-tests/src/test/resources/%s.properties", target))));
+
+    if ("".equals(properties.getProperty("selenium.server")))  {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      } else if (browser.equals(BrowserType.CHROME)) {
+        System.setProperty(properties.getProperty("chrome.name"), properties.getProperty("chrome.path"));
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+    wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    contactHelper = new ContactHelper(wd);
+    sessionHelper.login(properties.getProperty("web.admin-login"), properties.getProperty("web.admin-pass"));
+
+  }
+
+
+
+  public void stop() {
+    wd.quit();
+  }
+
+
+  public GroupHelper group() {
+    return groupHelper;
+  }
+
+  public NavigationHelper goTo() {
+    return navigationHelper;
+  }
+
+  public ContactHelper getContactHelper() {return contactHelper;}
+
+  public DbHelper db() {
+    return dbHelper;
+  }
+}
+*/
